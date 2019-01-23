15a7991ee2a3ffce70c164718f90bd7c57c988ed
Iyadh98 <35614094+Iyadh98@users.noreply.github.com>
2019-01-23 22:06:38 +0100

    Update main.js

--- word1/js/main.js
+++ word1/js/main.js
@@ -8,3 +8,13 @@
           });
         });
       });
+
+    $(document).ready(function(){
+        $("#myInput1").on("keyup", function() {
+          var value = $(this).val().toLowerCase();
+          $("#myTable1 tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+          });
+        });
+      });
+
